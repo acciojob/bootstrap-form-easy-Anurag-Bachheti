f56314050149587a5b488cc7a0794e3f23b4e4ff
Anurag Bachheti <157354714+Anurag-Bachheti@users.noreply.github.com>
2025-04-04 22:29:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-document.getElementById("my-form").addEventListener("submit", function(event) {
-    event.preventDefault();
-    if(!this.checkValidity()){
-        this.classList.add("was validated");
-    } else{
-        alert("Form submitted successfully!");
-    }
-});
+// document.getElementById("my-form").addEventListener("submit", function(event) {
+//     event.preventDefault();
+//     if(!this.checkValidity()){
+//         this.classList.add("was validated");
+//     } else{
+//         alert("Form submitted successfully!");
+//     }
+// });
