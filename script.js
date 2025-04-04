5a9dd1024aa950c45813e323fd338d4ca1d81f7e
Anurag Bachheti <157354714+Anurag-Bachheti@users.noreply.github.com>
2025-04-04 17:10:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your code here
+document.getElementById("my-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    if(!this.checkValidity()){
+        this.classList.add("was validated");
+    } else{
+        alert("Form submitted successfully!");
+    }
+});
